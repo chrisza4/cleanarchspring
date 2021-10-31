1fbd77edf5d9bc5e8c487bcebd15d5aa23acc7aa
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2021-10-31 09:02:46 +0700

    Init commit

---
+++ src/main/java/com/chrisza/cleandemo/CleandemoApplication.java
@@ -0,0 +1,13 @@
+package com.chrisza.cleandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleandemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleandemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrisza/cleandemo/CleandemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisza.cleandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleandemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
